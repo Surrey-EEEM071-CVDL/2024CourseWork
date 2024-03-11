4ea4ed2208cd7b4bb4c2962d2267520fec181648
Al-Hussein Abutaleb <61887086+AlHusseinA@users.noreply.github.com>
2024-03-11 11:33:14 +0000

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-python main.py \
+STUDENT_ID=kn00794 STUDENT_NAME="Jane Doe" python main.py \
 -s veri \
 -t veri \
 -a mobilenet_v3_small \
+--root /content \
 --height 224 \
 --width 224 \
 --optim amsgrad \
 --lr 0.0003 \
---max-epoch 30 \
---stepsize 10 20 \
---train-batch-size 256 \
+--max-epoch 10 \
+--stepsize 20 40 \
+--train-batch-size 64 \
 --test-batch-size 100 \
 --save-dir logs/mobilenet_v3_small-veri
